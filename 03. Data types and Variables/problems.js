a8ad92051500200ef9ae1f62f1f80a8ca9c8526e
ChrisChrisi <chris1cros@gmail.com>
2015-05-21 00:04:37 +0300

    Data types and variables problems solved

--- 03. Data types and Variables/problems.js
+++ 03. Data types and Variables/problems.js
@@ -3,7 +3,6 @@
  *    Assign all the possible JavaScript literals to different variables.
  */
 
-
 var arrayLiteral = ["One", "Two", "Three"];
 var booleanLiteralTrue = true;
 var booleanLiteralFalse = false;
@@ -12,8 +11,10 @@ var integaerLiteralOct = 015;
 var integaerLiteralHex = 0x155;
 var floatLiteral = 3.333;
 var objectLiteralHouse = {door: "wooden", walls: "bricks", windows: "glass"};
-
 var stringLiteral = "This is the first sentence. \n This is the second sentence.";
+var functionLiteral = function(){
+    console.log("Hello from the function!");
+};
 
 /**
  * ### Problem 2. Quoted text
@@ -29,55 +30,39 @@ console.log(quotedText);
 console.log("Second quoted text: ");
 console.log(anotherQuotedText);
 
+
 /**
  * ### Problem 3. Typeof variables
  *    Try typeof on all variables you created.
+ * ### Problem 4. Typeof null
+ *    Create null, undefined variables and try typeof on them.
  */
 
-console.log("\narray literal: ");
-console.log(arrayLiteral);
-console.log("typeof on array literal: ");
-console.log(typeof arrayLiteral);
+ function logAndTypeOf(val, str){
 
-console.log("\nboolean literal: ");
-console.log(booleanLiteralTrue);
-console.log("typeof on boolean literal: ");
-console.log(typeof booleanLiteralTrue);
-
-console.log("\ninteger literal: ");
-console.log(integerLiteralDec);
-console.log("typeof on integer literal: ");
-console.log(typeof integerLiteralDec);
-
-console.log("\nfloat literal: ");
-console.log(floatLiteral);
-console.log("typeof on float literal: ");
-console.log(typeof floatLiteral);
-
-console.log("\nobject literal: ");
-console.log(objectLiteralHouse);
-console.log("typeof on object literal: ");
-console.log(typeof objectLiteralHouse);
-
-console.log("\nstring literal: ");
-console.log(stringLiteral);
-console.log("typeof on string literal: ");
-console.log(typeof stringLiteral);
-
-/**
- * ### Problem 4. Typeof null
- *	Create null, undefined variables and try typeof on them.
-*/
+    console.log("\n"+str+" variable: ");
+    console.log(val);
+    console.log("typeof "+str+" variable: ");
+    console.log(typeof val);
+}
 
 var nullVar = null;
 var undefinedVar;
 
-console.log("\n variable with null value: ");
-console.log(nullVar);
-console.log("typeof variable with null value: ");
-console.log(typeof nullVar);
+var variables = {
+    array: arrayLiteral,
+    boolean: booleanLiteralTrue,
+    integer: integerLiteralDec,
+    float: floatLiteral,
+    object: objectLiteralHouse,
+    string: stringLiteral,
+    function: functionLiteral,
+    null: nullVar,
+    undefined: undefinedVar
+};
 
-console.log("\n variable with undefined value: ");
-console.log(undefinedVar);
-console.log("typeof variable with undefined value: ");
-console.log(typeof undefinedVar);
+for(var index in variables) {
+    if (variables.hasOwnProperty(index)) {
+        logAndTypeOf(variables[index], index);
+    }
+}
