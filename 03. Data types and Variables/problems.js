3ec36d64eb53fcad9188de49009668a847226f33
ChrisChrisi <chris1cros@gmail.com>
2015-05-23 21:07:38 +0300

    small fixes and reorder code

--- 03. Data types and Variables/problems.js
+++ 03. Data types and Variables/problems.js
@@ -1,23 +1,24 @@
 /**
- ### Problem 1. JavaScript literals
+ * ### Problem 1. JavaScript literals ###
  *    Assign all the possible JavaScript literals to different variables.
  */
 
-var arrayLiteral = ["One", "Two", "Three"];
-var booleanLiteralTrue = true;
-var booleanLiteralFalse = false;
-var integerLiteralDec = 5;
-var integaerLiteralOct = 015;
-var integaerLiteralHex = 0x155;
-var floatLiteral = 3.333;
-var objectLiteralHouse = {door: "wooden", walls: "bricks", windows: "glass"};
-var stringLiteral = "This is the first sentence. \n This is the second sentence.";
-var functionLiteral = function(){
-    console.log("Hello from the function!");
-};
+var arrayLiteral = ['One', 'Two', 'Three'],
+    booleanLiteralTrue = true,
+    booleanLiteralFalse = false,
+    integerLiteralDec = 5,
+    integaerLiteralOct = 015,
+    integaerLiteralHex = 0x155,
+    floatLiteral = 3.333,
+    objectLiteralHouse = {door: 'wooden', walls: 'bricks', windows: 'glass'},
+    stringLiteral = 'This is the first sentence. \n This is the second sentence.',
+    functionLiteral = function () {
+        console.log('Hello from the function!');
+    };
 
 /**
- * ### Problem 2. Quoted text
+ *
+ * ### Problem 2. Quoted text ###
  *    Create a string variable with quoted text in it.
  *    For example: `'How you doin'?', Joey said'.
  */
@@ -25,29 +26,29 @@ var functionLiteral = function(){
 var quotedText = '\'How you doin\'?\', Joey said\'';
 var anotherQuotedText = "'How you doin'?', Joey said'";
 
-console.log("First quoted text: ");
+console.log('First quoted text: ');
 console.log(quotedText);
-console.log("Second quoted text: ");
+console.log('Second quoted text: ');
 console.log(anotherQuotedText);
 
 
 /**
- * ### Problem 3. Typeof variables
+ * ### Problem 3. Typeof variables ###
  *    Try typeof on all variables you created.
- * ### Problem 4. Typeof null
+ * ### Problem 4. Typeof null ###
  *    Create null, undefined variables and try typeof on them.
  */
 
- function logAndTypeOf(val, str){
+function logAndTypeOf(val, str) {
 
-    console.log("\n"+str+" variable: ");
+    console.log('\n' + str + ' variable: ');
     console.log(val);
-    console.log("typeof "+str+" variable: ");
-    console.log(typeof val);
+    console.log('typeof ' + str + ' variable: ');
+    console.log(typeof(val));
 }
 
-var nullVar = null;
-var undefinedVar;
+var nullVar = null,
+    undefinedVar;
 
 var variables = {
     array: arrayLiteral,
@@ -61,7 +62,7 @@ var variables = {
     undefined: undefinedVar
 };
 
-for(var index in variables) {
+for (var index in variables) {
     if (variables.hasOwnProperty(index)) {
         logAndTypeOf(variables[index], index);
     }
