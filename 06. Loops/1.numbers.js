74ce97f59071cd18d34ae6673f846fc4d9bd9dbb
ChrisChrisi <chris1cros@gmail.com>
2015-05-27 11:03:14 +0300

    added negative naumber case

--- 06. Loops/1.numbers.js
+++ 06. Loops/1.numbers.js
@@ -7,12 +7,17 @@
 
 var allNumbersFrom1ToN = function(number){
     var i;
+    if(number >0){
     for(i = 1; i<= number; i++){
         console.log(i + ' ');
+    }} else {
+        for(i = 1; i>= number; i--){
+            console.log(i + ' ');
+        }
     }
 };
 
 console.log("Numbers from 1 to 5: ");
 allNumbersFrom1ToN(5);
-console.log("Numbers from 1 to 10: ");
-allNumbersFrom1ToN(10);
+console.log("Numbers from 1 to -5: ");
+allNumbersFrom1ToN(-5);
--- 06. Loops/2.numbersNotDivisible.js
+++ 06. Loops/2.numbersNotDivisible.js
@@ -4,17 +4,25 @@
  *    Write a script that prints all the numbers from `1` to `N`, that are not divisible by `3` and `7` at the same time
  */
 
-var notDivisibleBy3And7 = function(number){
+var notDivisibleBy3And7 = function (number) {
     var i = 1;
-    while(i <= number){
-        if(i % 3 !== 0 && i % 7 !== 0){
-            console.log(i + ' ');
+    if (number > 0) {
+        while (i <= number) {
+            if (i % 3 !== 0 && i % 7 !== 0) {
+                console.log(i + ' ');
+            }
+            i += 1;
         }
-        i+= 1;
+    } else {
+        while (i >= number) {
+            if (i % 3 !== 0 && i % 7 !== 0) {
+                console.log(i + ' ');
+            }
+            i -= 1;
+        }
     }
 };
-
 console.log("Numbers from 1 to 10 that are not divisible by 3 and 7: ");
 notDivisibleBy3And7(10);
 console.log("Numbers from 1 to 21 that are not divisible by 3 and 7: ");
-notDivisibleBy3And7(21);
+notDivisibleBy3And7(-10);
